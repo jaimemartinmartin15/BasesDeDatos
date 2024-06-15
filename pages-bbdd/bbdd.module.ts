aa7fb7dc7af77040b9571363bcccbf529c40c871
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-06-15 13:58:05 +0200

    Added assets, adapt paths, configure angular.json, fix styles and links.

--- pages-bbdd/bbdd.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { BBDDRoutingModule } from './bbdd-routing.module';
-
-@NgModule({
-  imports: [BBDDRoutingModule],
-})
-export class BBDDModule {}
--- src/app/sql/sql-exercises/sql-exercises.component.ts
+++ src/app/sql/sql-exercises/sql-exercises.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { CollapsibleModule, HeaderPrintComponent, ResponsiveLayoutComponent, TerminalCodeComponent } from '@jaimemartinmartin15/jei-devkit-angular-shared';
+import { RouterLink } from '@angular/router';
+import {
+  CollapsibleModule,
+  HeaderPrintComponent,
+  ResponsiveLayoutComponent,
+  TerminalCodeComponent,
+} from '@jaimemartinmartin15/jei-devkit-angular-shared';
 
 @Component({
   selector: 'app-sql-exercises',
   standalone: true,
-  imports: [CommonModule, HeaderPrintComponent, TerminalCodeComponent, CollapsibleModule, ResponsiveLayoutComponent],
+  imports: [CommonModule, HeaderPrintComponent, TerminalCodeComponent, CollapsibleModule, ResponsiveLayoutComponent, RouterLink],
   templateUrl: './sql-exercises.component.html',
   styleUrls: ['./sql-exercises.component.scss'],
 })
